e0acabb169a2d0829ff8d002d6d97c12c1137849
Ilya <ilyabikkulov@gmail.com>
2022-07-04 10:27:27 +0000

    Fix supervisor_start script

--- conf/ask_app/supervisor_start.sh
+++ conf/ask_app/supervisor_start.sh
@@ -13,9 +13,9 @@ if [ ! -e $APP_DIR/log/supervisor.log ]; then
     mkdir -p $APP_DIR/log
     touch $APP_DIR/log/supervisor.log
 fi
-
-/usr/bin/supervisord -c $SUPERVISOR_DIR/supervisord.conf
-
+if [[ -z "$(ps -ax | grep supervisord | grep -v grep)" ]]; then
+    /usr/bin/supervisord -c $SUPERVISOR_DIR/supervisord.conf 2>&1
+fi
 supervisorctl reread >/dev/null 2>&1
 supervisorctl update >/dev/null 2>&1
 supervisorctl restart ask >/dev/null 2>&1
