098c836595f4940e9d803610f33ee2b9bea3a4ff
Ilya <ilyabikkulov@gmail.com>
2022-07-05 06:33:16 +0000

    Add view for a question and its answers

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Question
 
 def test(request, question_id=None):
     return HttpResponse('OK')
+
+def question(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'qa/question.html', {'question': question})
