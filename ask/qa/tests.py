e85f58d0832e62e50f2ae2431bd955aa7b62a20e
Ilya <ilyabikkulov@gmail.com>
2022-07-08 08:06:39 +0000

    Add tests

--- ask/qa/tests.py
+++ ask/qa/tests.py
@@ -1,36 +1,51 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
-from .models import Question, User
-
-
-def create_test_user_if_not_exist():
-    try:
-        test_user = User.objects.get(username='test_user')
-    except User.DoesNotExist:
-        test_user = User.objects.create_user(username='test_user')
-    return test_user
-
-
-def create_question(title, text):
-    """
-    Create a question with the given 'title' and 'text';
-    Author of the question is 'test_user'.
-    """
-    author = create_test_user_if_not_exist()
-    return Question.objects.create(title=title, text=text, author=author)
+from .models import Question
+from .forms import AskForm, AnswerForm
 
 
 class QuestionViewTests(TestCase):
 
     def test_get_invalid_question(self):
-        """If question_id is invalid return 404"""
+        """If question_id is invalid return 404."""
         question_id = 0
         response = self.client.get(reverse('question', args=(question_id,)))
         self.assertEqual(response.status_code, 404)
 
     def test_get_valid_question(self):
-        """If question_id is valid return 200"""
-        question = create_question(title='test title', text='test text')
+        """If question_id is valid return 200."""
+        question = Question.objects.create(title='title1', text='text1')
         response = self.client.get(reverse('question', args=(question.id,)))
         self.assertEqual(response.status_code, 200)
+
+
+class AnswerFormTests(TestCase):
+
+    def test_valid_data(self):
+        question = Question.objects.create(title='title1', text='text1')
+        form = AnswerForm(data={
+            'text': 'text1',
+            'question': question.id
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_data(self):
+        """Empty form data raises ValidationError."""
+        form = AnswerForm(data={})
+        self.assertFalse(form.is_valid())
+
+
+class AskFormTests(TestCase):
+
+    def test_valid_data(self):
+        form = AskForm(data={
+            'title': 'title1',
+            'text': 'text1'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_data(self):
+        """Empty form data raises ValidationError."""
+        form = AskForm(data={})
+        self.assertFalse(form.is_valid())
